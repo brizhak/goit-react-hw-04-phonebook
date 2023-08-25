db8eb79b3f5e9c6132d9fa730fab7171a5ede729
brizhak <ada.adelina17@gmail.com>
2023-08-25 15:14:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 import style from './App.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts, setContacts } from 'redux/contactsSlice';
 
 const App = () => {
-  const contacts = useSelector(selectContacts);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const savedData = localStorage.getItem('contacts');
-    if (savedData) {
-      dispatch(setContacts(JSON.parse(savedData)));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div className={style.container}>
       <h1>Phonebook</h1>
